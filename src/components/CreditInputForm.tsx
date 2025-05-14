2ba8972c513c44d54cb4deed79dab7b3700c1c87
Pacho <balashovpashabk@gmail.com>
2025-05-14 16:08:34 +0300

    hren

--- src/components/CreditInputForm.tsx
+++ src/components/CreditInputForm.tsx
@@ -70,65 +70,67 @@ const CreditInputForm: React.FC<CreditInputFormProps> = ({ onExport }) => {
 
   return (
     <div>
-      <div className="forms-container">
-        <div className={`form-container ${schedule ? 'compact' : ''}`}>
-          <div className="form-header">Данные кредита</div>
-          <Form>
-            <div className="form-row">
-              <Form.Group className="form-group">
-                <Form.Label className="form-label">Сумма кредита (руб.)</Form.Label>
-                <Form.Control
-                  type="number"
-                  value={loanAmount}
-                  onChange={(e) => setLoanAmount(Number(e.target.value))}
-                  placeholder="Введите сумму"
-                  className="form-control"
-                />
-              </Form.Group>
-              <Form.Group className="form-group">
-                <Form.Label className="form-label">Годовая ставка (%)</Form.Label>
-                <Form.Control
-                  type="number"
-                  value={annualRate}
-                  onChange={(e) => setAnnualRate(Number(e.target.value))}
-                  placeholder="Введите ставку"
-                  className="form-control"
-                />
-              </Form.Group>
-              <Form.Group className="form-group">
-                <Form.Label className="form-label">Срок кредита (месяцы)</Form.Label>
-                <Form.Control
-                  type="number"
-                  value={termMonths}
-                  onChange={(e) => setTermMonths(Number(e.target.value))}
-                  placeholder="Введите срок"
-                  className="form-control"
-                />
-              </Form.Group>
-              <Form.Group className="form-group">
-                <Form.Label className="form-label">Тип платежа</Form.Label>
-                <Form.Select
-                  value={paymentType}
-                  onChange={(e) => handlePaymentTypeChange(e.target.value)}
-                  className="form-select"
-                >
-                  <option value="annuity">Аннуитетный</option>
-                  <option value="differentiated">Дифференцированный</option>
-                </Form.Select>
-              </Form.Group>
-              <Button variant="primary" onClick={handleCalculate} className="btn-primary">
-                Рассчитать
-              </Button>
-            </div>
-          </Form>
-        </div>
-
-        {schedule && (
-          <div className="form-container compact">
-            <div className="form-header">Добавить дополнительный платеж</div>
-            <RecalculationInputForm onAddPayment={handleAddPayment} maxMonth={schedule.loan.termMonths} />
+      <div className="forms-wrapper">
+        <div className="forms-container">
+          <div className={`form-container credit-data ${schedule ? '' : ''}`}>
+            <div className="form-header">Данные кредита</div>
+            <Form>
+              <div className="form-row">
+                <Form.Group className="form-group">
+                  <Form.Label className="form-label">Сумма кредита (руб.)</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={loanAmount}
+                    onChange={(e) => setLoanAmount(Number(e.target.value))}
+                    placeholder="Введите сумму"
+                    className="form-control"
+                  />
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Form.Label className="form-label">Годовая ставка (%)</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={annualRate}
+                    onChange={(e) => setAnnualRate(Number(e.target.value))}
+                    placeholder="Введите ставку"
+                    className="form-control"
+                  />
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Form.Label className="form-label">Срок кредита (месяцы)</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={termMonths}
+                    onChange={(e) => setTermMonths(Number(e.target.value))}
+                    placeholder="Введите срок"
+                    className="form-control"
+                  />
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Form.Label className="form-label">Тип платежа</Form.Label>
+                  <Form.Select
+                    value={paymentType}
+                    onChange={(e) => handlePaymentTypeChange(e.target.value)}
+                    className="form-select"
+                  >
+                    <option value="annuity">Аннуитетный</option>
+                    <option value="differentiated">Дифференцированный</option>
+                  </Form.Select>
+                </Form.Group>
+                <Button variant="primary" onClick={handleCalculate} className="btn-primary">
+                  Рассчитать
+                </Button>
+              </div>
+            </Form>
           </div>
-        )}
+
+          {schedule && (
+            <div className="form-container extra-payment">
+              <div className="form-header">Добавить дополнительный платеж</div>
+              <RecalculationInputForm onAddPayment={handleAddPayment} maxMonth={schedule.loan.termMonths} />
+            </div>
+          )}
+        </div>
       </div>
 
       {schedule && (
